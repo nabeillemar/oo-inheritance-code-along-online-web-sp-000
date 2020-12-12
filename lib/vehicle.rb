140b0f8b9b0a0748234ef7a7aad9ee32d0ee16a4
Nabeil Lemar <nabeillemar@gmail.com>
2020-12-12 12:54:20 +0000

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,17 @@
-class Vehicle
-
+class Vehicle #(Super class)
+  attr_accessor :wheel_size, :wheel_number
+  
+  def initialize(wheel_size, wheel_number) #gets initialized 
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end 
+  
+  def go #Instance methods
+    "vrrrrrrrooom!"
+  end 
+  
+  def fill_up_tank #Instance methods 
+    "filling up!"
+  end 
+  
 end
